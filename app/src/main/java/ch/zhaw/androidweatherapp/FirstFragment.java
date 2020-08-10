08a68fa38ec3832e0bfb35a62c2f1def65eb97ea
Urs Albisser <u.albisser@gmail.com>
2020-08-10 16:37:28 +0200

    code clean-up & commenting of FirstFragment.java

--- app/src/main/java/ch/zhaw/androidweatherapp/FirstFragment.java
+++ app/src/main/java/ch/zhaw/androidweatherapp/FirstFragment.java
@@ -1,6 +1,5 @@
 package ch.zhaw.androidweatherapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -8,79 +7,103 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.json.JSONObject;
-
-import cz.msebera.android.httpclient.Header;
-
 public class FirstFragment extends Fragment {
+
+
+
 
     // == fields ==
     private String newCity;
 
+
+
+
+    // == main public methods ==
+
+    /**
+     * onCreateView()
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return  attacheToRoot: true/false
+     */
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // Inflate the layout for this fragment
+        // Inflate the layout "R.layout.fragment_first" for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
+
+    /**
+     * onViewCreated()
+     * Handle manual city search by search query / EditText.
+     * Handle Button for current location.
+     * @param view  View
+     * @param savedInstanceState    Bundle
+     */
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final EditText edittext = (EditText)view.findViewById(R.id.queryET);
-//        final Button submit = (Button)view.findViewById(R.id.submit);
+        /*
+        Handle Button for current location
+        */
+        View geolocationButton = view.findViewById(R.id.geoloc_btn);
 
-        edittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        geolocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // set city search flag = false
+                MainActivity.setIsCitySearch(false);
+
+                // navigation to second fragment
+                navigateToSecondFragment();
+            }
+        });
+
+         /*
+         Handle manual city search by search query / EditText
+         */
+        final EditText editText = view.findViewById(R.id.citySearchQuery);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                    //do what you want on the press of 'done'
 
+                    // set city search flag = true
                     MainActivity.setIsCitySearch(true);
 
-                    newCity = edittext.getText().toString();
+                    // handle entered String
+                    newCity = editText.getText().toString();
                     Log.d("Debug", "newCity = " + newCity);
                     MainActivity.setCity(newCity);
 
-                    NavHostFragment.findNavController(FirstFragment.this)
-                            .navigate(R.id.action_FirstFragment_to_SecondFragment);
-
-//                    submit.performClick();
+                    // navigation to second fragment
+                    navigateToSecondFragment();
                 }
                 return false;
             }
         });
+    }
 
 
-        // Button for current location§
-        View geolocationButton = view.findViewById(R.id.geoloc_btn);
-        view.findViewById(R.id.geoloc_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                MainActivity.setIsCitySearch(false);
-
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+    /**
+     * navigateToSecondFragment()
+     * Handles the navigation to the second fragment.
+     */
+    private void navigateToSecondFragment() {
+        NavHostFragment.findNavController(FirstFragment.this)
+                .navigate(R.id.action_FirstFragment_to_SecondFragment);
     }
 }
 
